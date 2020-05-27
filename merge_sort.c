d8ecc2af22d50cb9773d18b41aaa294ce6f93328
Clovis Wanderley Bertholini Sobrinho <clovis.cwbs@gmail.com>
2020-05-27 11:33:30 -0400

    still trying

--- merge_sort.c
+++ merge_sort.c
@@ -9,7 +9,8 @@
 const int QTD = 15;
 
 //functions declarations
-int merge_sort(int v[]);
+void merge_sort(int v[], int a, int b);
+void merge(int v[], int a, int mid, int mid1, int b);
 
 int main(void)
 {
@@ -28,23 +29,63 @@ int main(void)
 	printf("\n");
 	
 	//sort numbers array with merge sort algorithm
-	merge_sort(numbers);
+	merge_sort(numbers, 0, QTD); 
+	return 0;
+}
+
+void merge_sort(int v[], int a, int b)
+{
+	//TODO
+	int mid = (a + b) / 2;
+	if (v[a] == v[b])
+	{
+		break;
+	}
+	else (v[a] < v[b])
+	{
+		merge_sort(v, a, mid);
+		merge_sort(v, mid + 1, b);
+		merge(v, a, mid, mid + 1, b);
+	}
+}
+
+void merge(int v[], int a, int mid, int mid1, int b)
+{
+	int x[b];
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	while (i < mid && j < b)
+	{
+		if (v[i] < v[j])
+		{
+			x[k] = v[i];
+			i++;
+		}
+		else
+		{
+			x[k] = v[j];
+			j++;
+		}
+		k++;
+	}
+	while (i < mid)
+	{
+		x[k] = v[i];
+		i++;
+		k++;
+	}
+	while (j < b)
+	{
+		x[k] = v[j];
+		j++;
+		k++;
+	}
 
 	//print numbers sorted
 	for (int i = 0; i < QTD; i++)
 	{
-		printf("%i", v[i]);
+		printf("%i", x[i]);
 		printf("\n");
 	}
-	return 0;
-}
-
-int merge_sort(int v[])
-{
-	//TODO
-	if (i = 1)
-	{}
-	else
-	{
-		merge_sort(	
 }
