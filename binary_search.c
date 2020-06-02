311527a354fb6dfcef7d4cc36a32bf86ee33b23e
Clovis Wanderley Bertholini Sobrinho <clovis.cwbs@gmail.com>
2020-06-01 22:57:18 -0400

    Harvard X --> cs50 --> Week 3 --> Algorithms --> Binary Search --> still trying

--- binary_search.c
+++ binary_search.c
@@ -8,16 +8,35 @@
 
 const int MAX = 15;
 
+//functions declarations
+void merge_sort(int v[], int a, int b);
+void merge(int v[], int a, int mid, int b);
 int binary_search(int v[], int n, int a, int b);
 
 int main(void)
 {
-    int x = rand();
+    int x = 288; //rand();
     int numbers[MAX];
     for (int i = 0; i < MAX; i++)
     {
         numbers[i] = rand();
     }
+    //print numbers unsorted
+	for (int i = 0; i < MAX; i++)
+	{
+		printf("%i", numbers[i]);
+		printf("\n");
+	}
+	printf("\n");
+    //sort numbers
+    merge_sort(numbers, 0, MAX);
+    //print numbers sorted
+	for (int i = 0; i < MAX; i++)
+	{
+		printf("%i", numbers[i]);
+		printf("\n");
+	}
+    //find number x
     int result = binary_search(numbers, x, 0, MAX - 1);
     if (result == 1)
     {
@@ -30,18 +49,75 @@ int main(void)
     return 0;
 }
 
+void merge_sort(int v[], int a, int b)
+{
+	//divide array in subarrays
+	int mid = (a + b) / 2;
+	if (a < b)
+	{
+		merge_sort(v, a, mid);
+		merge_sort(v, mid + 1, b);
+		merge(v, a, mid, b);
+	}
+}
+
+void merge(int v[], int a, int mid, int b)
+{
+	int x[MAX];
+	int i = a;
+	int j = mid + 1;
+	int k = a;
+	while (i <= mid && j <= b)
+	{
+		if (v[i] < v[j])
+		{
+			x[k] = v[i];
+			i++;
+		}
+		else
+		{
+			x[k] = v[j];
+			j++;
+		}
+		k++;
+	}
+	if (j > b)
+	{
+		while (i <= mid)
+		{
+			x[k] = v[i];
+			i++;
+			k++;
+		}
+	}
+	else
+	{
+		while (j <= b)
+		{
+			x[k] = v[j];
+			j++;
+			k++;
+		}
+	}
+	for (k = a; k <= b; k++)
+	{
+		v[k] = x[k];
+	}
+}
+
+
 int binary_search(int v[], int n, int a, int b)
 {
     if (n == v[(b - a) / 2])
     {
         return 1;
     }
-    else if (n < v[(b - a) / 2])
+    else if (n < v[(b - a) / 2] && a != b)
     {
         b = (b - a) / 2;
         binary_search(v, n, a, b);
     }
-    else if (n > v[(b - a) / 2])
+    else if (n > v[(b - a) / 2] && a != b)
     {
         a = (b - a) / 2;
         binary_search(v, n, a, b);
@@ -50,5 +126,4 @@ int binary_search(int v[], int n, int a, int b)
     {
         return 0;
     }
-    
 }
