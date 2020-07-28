b28190650f18a83078c24434a38f1fc23d09279d
Clovis Wanderley Bertholini Sobrinho <clovis.cwbs@gmail.com>
2020-07-27 20:55:35 -0400

    C programming course

---
+++ test_5_Dartmouth_IMTx.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+    int i;
+    for(i = 0; i < 3; i++)
+        printf("C is fun!\n");
+    printf("\n");
+    for(i = 5; i > 0; i--)
+        printf("We can do everything with it!\n");
+    return 0;
+}
