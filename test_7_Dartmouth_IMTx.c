a3641fab268e199cfa69453bebccc33dcaeb6f7b
Clovis Wanderley Bertholini Sobrinho <clovis.cwbs@gmail.com>
2020-07-29 23:12:58 -0400

    C programming course

---
+++ test_7_Dartmouth_IMTx.c
@@ -0,0 +1,19 @@
+// preprocessor directive
+#include <stdio.h>
+
+// main function
+int main(void){
+    
+    // variable declarations
+    int i;
+    
+    // executable statments
+    printf("I ");
+    for (i = 0; i < 3; i++) {
+        printf("love ");
+    }
+    printf("programming!");
+    
+    // return statment
+    return 0;
+}
