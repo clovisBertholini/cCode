b78923719c5efc7c90854b160a6d7839aab488fc
Clovis Wanderley Bertholini Sobrinho <clovis.cwbs@gmail.com>
2020-08-03 18:52:37 -0400

    C programming course

--- test_11_Dartmouth_IMTx.c
+++ test_11_Dartmouth_IMTx.c
@@ -2,9 +2,9 @@
 int main(void){
     int numberOfRobots, enginePower, resistance, weight, height;
     int armyPower = 0;
-    scanf("%d", numberOfRobots);
+    scanf("%d", &numberOfRobots);
     for (int i = 0; i < numberOfRobots; i++){
-        scanf("%d%d%d%d", height, weight, resistance, enginePower);
+        scanf("%d%d%d%d", &height, &weight, &resistance, &enginePower);
         armyPower += (enginePower + resistance) * (weight - height);
     }
     printf("%d", armyPower);
