6649062a099778d209f104e9e0c7846ece303da4
Clovis Wanderley Bertholini Sobrinho <clovis.cwbs@outlook.com>
2021-02-13 22:15:37 -0400

    getString finished

--- getChar1.c
+++ getChar1.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-void getString(char *v);
+void getString(char *v, int t);
 
 const int TEXTO = 10;
 
 int main(void){
 	char texto[TEXTO];
 	printf("texto: ");
-	getString(texto);
+	getString(texto, TEXTO);
 	printf("\n\t-> %s\n", texto);
 }
 
-void getString(char *v){
+void getString(char *v, int t){
 	int count = 0;
-	while ((v[count] = getchar()) != '\n' && count < (TEXTO - 2))
+	while ((v[count] = getchar()) != '\n' && count < (t - 2))
 		++count;
 	v[++count] = '\0';
 }
